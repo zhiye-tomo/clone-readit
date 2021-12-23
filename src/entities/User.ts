ee394442b32b8aec13562a3223bdf06152c6562f
zhiye-tomo <haochide1128@gmail.com>
2021-12-23 09:09:32 +0900

    authentication done

---
+++ src/entities/User.ts
@@ -0,0 +1,47 @@
+import { IsEmail, Length } from "class-validator";
+import {Entity, PrimaryGeneratedColumn, Column, BaseEntity, Index, CreateDateColumn, UpdateDateColumn, BeforeInsert } from "typeorm";
+import bcrypt from  "bcrypt";
+import { classToPlain, Exclude } from "class-transformer";
+
+@Entity('users')
+export class User extends BaseEntity {
+    constructor(user: Partial<User>) {
+        super();
+        Object.assign(this, user);
+    };
+    
+    @Exclude()
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Index()
+    @IsEmail()
+    @Column({ unique: true })
+    email: string;
+
+    @Index()
+    @Length(3, 255)
+    @Column({ unique: true })
+    username: string;
+        
+    @Exclude()
+    @Length(6, 255)
+    @Column()
+    password: string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+
+    @BeforeInsert()
+    async hashPassword() {
+      this.password = await bcrypt.hash(this.password, 6);
+    };
+
+    toJSON() {
+        return classToPlain(this)
+    };
+
+}
---
+++ src/server.ts
@@ -0,0 +1,32 @@
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import express from "express";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+import authRoutes from './routes/auth';
+
+import trim from './middleware/trim';
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(trim);
+app.use(cookieParser());
+
+app.get('/', (_, res) => res.send('Hello World'));
+app.use('/api/auth', authRoutes);
+
+app.listen(3001, async () => {
+    console.log('Server running at http://localhost:3001');
+    try {
+        await createConnection()
+        console.log('Database connected!');
+    } catch (err) {
+        console.log(err);
+    }
+});
