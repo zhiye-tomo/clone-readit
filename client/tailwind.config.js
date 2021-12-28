1d3a5ced5b105b405009c2317996f5b5756b4bc6
zhiye-tomo <haochide1128@gmail.com>
2021-12-29 03:33:35 +0900

    [client] register page is added

---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      spacing: {
+        70:  '17.5rem',
+      }
+    },
+  },
+  plugins: [],
+}
